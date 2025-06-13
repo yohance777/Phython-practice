f3a3255237c39f33f19ea52af8f4ea2f3e019a9c
yohance777 <rmadhavi.kamble@gmail.com>
2025-06-13 05:50:36 +0000

    search_everything

---
+++ regex.py/re_serch.py
@@ -0,0 +1,5 @@
+import re
+a = "hi! i am yohance i          vngrkyurnciregtnremcrgtyrtieryiginhucerhviergnicrmfnehvrugcnirehc8ueriohnmteiyyyyyyyyyyyyyu8v485ytort8hcofiyfomf"
+c = r"i"
+b = re.findall(c, a)
+print(b)

da7008003fb253e522630ab06d47d0cda6fbd264
yohance777 <rmadhavi.kamble@gmail.com>
2025-06-13 04:44:42 +0000

    dictionay_data_type

---
+++ dict.py
@@ -0,0 +1,12 @@
+student = {
+    "name" : "yohance",
+    
+    "std" : 8, 
+    
+    "sport" : "chess"
+
+}
+student["sport"] = "vollyball"
+print(student["std"])
+ 
+  
---
+++ modify_set_remove.py
@@ -0,0 +1,3 @@
+my_set = {1,2,3,4,5,6,7,8,9,0}
+my_set.remove(9)
+print(my_set)
